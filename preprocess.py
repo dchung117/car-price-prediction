d0f0c21ec2379f818303bff6a0a27e156a2bd791
dchung117 <dchung0330@gmail.com>
2023-08-01 03:46:28 +0000

    wrapped preprocessing steps in function. included documentation.

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,16 @@ FEAT_COLS = [
 ]
 
 TARGET_COL = "current price"
-if __name__ == "__main__":
+
+def preprocess_data(data_file: pathlib.Path) -> tuple[tf.Tensor, tf.Tensor]:
+    """
+    Prepare data for training.
+    
+    :param: data_file - path to training data csv.
+    :dtype: pathlib.Path
+    :return: feature and target tensors for training.
+    :rtype: tuple[tf.Tensor, tf.Tensor]
+    """
     # Load data
     data_file = pathlib.Path("data/train.csv")
     df = pd.read_csv(data_file)
@@ -30,4 +39,11 @@ if __name__ == "__main__":
     # Get data and features
     X = data[:, :-1]
     y = tf.expand_dims(data[:, -1], axis=-1)
+    
+    return X, y
+
+if __name__ == "__main__":
+    # Load data
+    data_file = pathlib.Path("data/train.csv")
+    X, y = preprocess_data(data_file)
     print(X.shape, y.shape)
