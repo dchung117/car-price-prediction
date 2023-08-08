9a79b779eb5ea44210bc39cce296cf14880f7aed
dchung117 <dchung0330@gmail.com>
2023-08-08 02:12:55 +0000

    edited setup files to complete setup.

---
+++ setup_tf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate car-prediction
+
+# configure cudnn, ld_library path
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$CUDNN_PATH/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
