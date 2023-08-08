9be7fb2ec879bb4f63c85c62c2f379856e940328
dchung117 <dchung0330@gmail.com>
2023-08-08 02:46:33 +0000

    added mapping for loss functions.

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import tensorflow as tf
 from src.preprocess import preprocess_data
 from src.model import get_model
 
+LOSSES = {
+    "mse": tf.keras.losses.MeanSquaredError,
+    "mae": tf.keras.losses.MeanAbsoluteError,
+    "huber": tf.keras.losses.Huber
+}
+
+def train(model: tf.keras.Model):
+    pass
+
 if __name__ == "__main__":
     # Load, preprocess data
     data_file = pathlib.Path("data/train.csv")
@@ -11,4 +20,7 @@ if __name__ == "__main__":
     
     # Create model
     model = get_model(X_normalizer, n_feats=X.shape[1])
+    model.compile(loss=LOSSES["mse"])
     tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
+
+
