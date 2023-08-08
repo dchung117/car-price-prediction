6d9c4ce9d30c777bc0038034b7f5b3a18a777ffc
dchung117 <dchung0330@gmail.com>
2023-08-08 02:33:25 +0000

    added input layer.

--- model.py
+++ model.py
@@ -1,16 +1,20 @@
 import tensorflow.keras as keras
-from tensorflow.keras.layers import Normalization, Dense
+from tensorflow.keras.layers import Normalization, Dense, InputLayer
 
-def get_model(normalizer: Normalization) -> keras.Model:
+def get_model(normalizer: Normalization, n_feats: int) -> keras.Model:
     """
     Define linear model for car price prediction.
     
     :param: normalizer: Normalization layer for input features
+    :dtype: Normalization
+    :param: n_feats: Number of input features
+    :dtype: int
     :return: Linear model for car price prediction
     :rtype: keras.Model
     """
     
     model = keras.Sequential()
+    model.add(InputLayer(input_shape=(8, )))
     model.add(normalizer)
     model.add(Dense(1))
     print("Model Summary: ")
