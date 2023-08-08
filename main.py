8e924c00d5048f7ea9e0b146af5b9fe3c6012658
dchung117 <dchung0330@gmail.com>
2023-08-08 02:37:12 +0000

    creating input layer.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pathlib
 import tensorflow as tf
 
-from preprocess import preprocess_data
-from model import get_model
+from src.preprocess import preprocess_data
+from src.model import get_model
 
 if __name__ == "__main__":
     # Load, preprocess data
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     X, y, X_normalizer, y_normalizer = preprocess_data(data_file)
     
     # Create model
-    model = get_model(X_normalizer)
+    model = get_model(X_normalizer, n_feats=X.shape[1])
     tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
