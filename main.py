f7ccc15fc0034a85efa518c9deb60a7bea8e7fef
dchung117 <dchung0330@gmail.com>
2023-08-11 04:04:51 +0000

    create train/val/test/split function.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from src.preprocess import preprocess_data
+from src.preprocess import preprocess_data, train_val_test_split
 from src.model import get_model
 
 LOSSES = {
@@ -13,10 +13,6 @@ LOSSES = {
     "mae": tf.keras.losses.MeanAbsoluteError,
     "huber": tf.keras.losses.Huber
 }
-
-TRAIN_FRACTION = 0.8
-VAL_FRACTION = 0.1
-TEST_FRACTION = 0.1
 
 def train(model: tf.keras.Model, X: tf.Tensor, y: tf.Tensor, 
     batch_size: int = 32, n_epochs: int = 100) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
@@ -74,14 +70,14 @@ if __name__ == "__main__":
     X, y, X_normalizer, y_normalizer = preprocess_data(data_file)
     
     # Split data into train, val, test
-    X_train, y_train = X[:int(len(X)*TRAIN_FRACTION)], y[:int(len(X)*TRAIN_FRACTION)]
-    X_val, y_val = X[int(len(X)*TRAIN_FRACTION):int(len(X)*(TRAIN_FRACTION+VAL_FRACTION))], \
-        y[int(len(X)*TRAIN_FRACTION):int(len(X)*(TRAIN_FRACTION+VAL_FRACTION))]
-    X_test, y_test =X[-int(len(X)*TEST_FRACTION):], y[-int(len(X)*TEST_FRACTION):]
+    X_train, y_train, X_val, y_val, X_test, y_test = train_val_test_split(X, y)
     print("Training size: ", X_train.shape[0])
     print("Validation size: ", X_val.shape[0])
     print("Test size: ", X_test.shape[0])
-
+    exit()
+    # Re-normalize to training set
+    X_normalizer.adapt(X_train)
+    y_normalizer.adapt(y_train)
 
     # Create model
     model = get_model(X_normalizer, n_feats=X.shape[1])
@@ -91,7 +87,7 @@ if __name__ == "__main__":
     tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
 
     # Train model
-    model, hist = train(model, X, y)
+    model, hist = train(model, X_train, y_train)
     save_plot(
         np.arange(len(hist.history["loss"])),
         hist.history["loss"],
@@ -108,6 +104,6 @@ if __name__ == "__main__":
     )
 
     # Evaluate model
-    model.evaluate(X, y, verbose=1)
+    model.evaluate(X_test, y_test, verbose=1)
 
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -51,6 +51,33 @@ def preprocess_data(data_file: pathlib.Path) \
 
     return X, y, X_normalizer, y_normalizer
 
+def train_val_test_split(X: tf.Tensor, y: tf.Tensor,
+    train_fraction: float = 0.8, val_fraction: float = 0.1, test_fraction: float = 0.1) \
+    -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+    """
+    Split features and targets into training, validation, test sets.
+    
+    :param: X - feature tensor
+    :dtype: tf.Tensor
+    :param: y - target tensor
+    :dtype: tf.Tensor
+    :param: train_fraction - training set split (def. 0.8)
+    :dtype: float
+    :param: val_fraction - validation set split (def. 0.1)
+    :dtype: float
+    :param: test_fraction - test set split (def. 0.1)
+    :dtype: float
+    :return: Training, validation, test features and targets
+    :rtype: tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]
+    """
+    assert train_fraction + val_fraction + test_fraction == 1, "Train, val, test fractions must sum to 1."
+    X_train, y_train = X[:int(len(X)*train_fraction)], y[:int(len(X)*train_fraction)]
+    X_val, y_val = X[int(len(X)*train_fraction):int(len(X)*(train_fraction+val_fraction))], \
+        y[int(len(X)*train_fraction):int(len(X)*(train_fraction+val_fraction))]
+    X_test, y_test =X[-int(len(X)*test_fraction):], y[-int(len(X)*test_fraction):]
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
 if __name__ == "__main__":
     # Load data
     data_file = pathlib.Path("data/train.csv")
