05612fec8cc5a37ed1e6b926f4cc1136dc9c3206
dchung117 <dchung0330@gmail.com>
2023-08-11 04:58:45 +0000

    added tf datasets.

--- main.py
+++ main.py
@@ -15,31 +15,29 @@ LOSSES = {
     "huber": tf.keras.losses.Huber
 }
 
-def train(model: tf.keras.Model, X: tf.Tensor, y: tf.Tensor, 
+def train(model: tf.keras.Model, train_data: tf.data.Dataset, 
     batch_size: int = 32, n_epochs: int = 100,
-    val_data: Optional[tuple[tf.Tensor, tf.Tensor]] = None) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
+    val_data: Optional[tf.data.Dataset] = None) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
     """
     Function to train a model.
     
     :param: model: Keras model to train.
     :dtype: tf.keras.Model
-    :param: X: Model training input tensor.
-    :dtype: tf.Tensor
-    :param: y: Model training target tensor.
-    :dtype: tf.Tensor
+    :param: train_data: Training data
+    :dtype: tf.data.Dataset
     :param: batch_size: Training batch size (def. 32).
     :dtype: int
     :param: n_epochs: Number of training epochs (def. 100).
     :dtype: int
     :param: val_data: Optional validation data
-    :dtype: Optional[tuple[tf.Tensor, tf.Tensor]]
+    :dtype: Optional[tf.data.Dataset]
     :return: trained model and training history
     :rtype: tuple[tf.keras.Model, tf.keras.callbacks.History]
     """
     if val_data:
-        hist = model.fit(X, y, validation_data=val_data, epochs=n_epochs, batch_size=batch_size, verbose=1)
+        hist = model.fit(train_data, validation_data=val_data, epochs=n_epochs, batch_size=batch_size, verbose=1)
     else:
-        hist = model.fit(X, y, epochs=n_epochs, batch_size=batch_size, verbose=1)
+        hist = model.fit(train_data, epochs=n_epochs, batch_size=batch_size, verbose=1)
     return model, hist
     
 if __name__ == "__main__":
@@ -48,14 +46,22 @@ if __name__ == "__main__":
     X, y, X_normalizer, y_normalizer = preprocess_data(data_file)
     
     # Split data into train, val, test
-    X_train, y_train, X_val, y_val, X_test, y_test = train_val_test_split(X, y)
-    print("Training size: ", X_train.shape[0])
-    print("Validation size: ", X_val.shape[0])
-    print("Test size: ", X_test.shape[0])
+    train_dataset, val_dataset, test_dataset = train_val_test_split(X, y)
+    print("Training size: ", len(train_dataset))
+    print("Validation size: ", len(val_dataset))
+    print("Test size: ", len(test_dataset))
 
     # Re-normalize to training set
-    X_normalizer.adapt(X_train)
-    y_normalizer.adapt(y_train)
+    X_normalizer.adapt(
+        tf.convert_to_tensor(
+        np.concatenate([x for x,y in train_dataset], axis=0)
+        )
+    )
+    y_normalizer.adapt(
+        tf.convert_to_tensor(
+        np.concatenate([y for x,y in train_dataset], axis=0)
+        )
+    )
 
     # Create model
     model = get_mlp(X_normalizer, n_feats=X.shape[1],
@@ -66,7 +72,7 @@ if __name__ == "__main__":
     tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
 
     # Train model
-    model, hist = train(model, X_train, y_train, val_data=(X_val, y_val))
+    model, hist = train(model, train_dataset, val_data=val_dataset)
     save_plot(
         np.arange(len(hist.history["loss"])),
         [hist.history["loss"], hist.history["val_loss"]],
@@ -87,10 +93,11 @@ if __name__ == "__main__":
     )
 
     # Evaluate model on test set
-    model.evaluate(X_test, y_test, verbose=1)
+    model.evaluate(test_dataset, verbose=1)
 
     # Get prediction on test example
-    x_ex, y_ex = X_test[0], y_test[0].numpy()[0]
+    x_batch, y_batch = next(iter(test_dataset))
+    x_ex, y_ex = x_batch[0], y_batch[0].numpy()[0]
     y_pred = predict(x_ex, model)
     print("Car price prediction: ", y_pred)
     print("Prediction error: ", y_pred - y_ex)
--- src/preprocess.py
+++ src/preprocess.py
@@ -52,8 +52,9 @@ def preprocess_data(data_file: pathlib.Path) \
     return X, y, X_normalizer, y_normalizer
 
 def train_val_test_split(X: tf.Tensor, y: tf.Tensor,
-    train_fraction: float = 0.8, val_fraction: float = 0.1, test_fraction: float = 0.1) \
-    -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+    train_fraction: float = 0.8, val_fraction: float = 0.1, test_fraction: float = 0.1,
+    batch_size: int = 32) \
+    -> tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
     """
     Split features and targets into training, validation, test sets.
     
@@ -67,8 +68,10 @@ def train_val_test_split(X: tf.Tensor, y: tf.Tensor,
     :dtype: float
     :param: test_fraction - test set split (def. 0.1)
     :dtype: float
+    :param: batch_size - batch size for datasets (def. 32)
+    :dtype: int
     :return: Training, validation, test features and targets
-    :rtype: tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]
+    :rtype: tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]
     """
     assert train_fraction + val_fraction + test_fraction == 1, "Train, val, test fractions must sum to 1."
     X_train, y_train = X[:int(len(X)*train_fraction)], y[:int(len(X)*train_fraction)]
@@ -76,7 +79,16 @@ def train_val_test_split(X: tf.Tensor, y: tf.Tensor,
         y[int(len(X)*train_fraction):int(len(X)*(train_fraction+val_fraction))]
     X_test, y_test =X[-int(len(X)*test_fraction):], y[-int(len(X)*test_fraction):]
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    
+    # Create tf datasets, shuffle
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)) \
+        .shuffle(buffer_size=8, reshuffle_each_iteration=True).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val)) \
+        .shuffle(buffer_size=8, reshuffle_each_iteration=True).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test)) \
+        .shuffle(buffer_size=200, reshuffle_each_iteration=True).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    
+    return train_dataset, val_dataset, test_dataset
 
 if __name__ == "__main__":
     # Load data
